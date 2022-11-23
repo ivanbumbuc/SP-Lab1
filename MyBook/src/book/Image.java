65be617e48b1cc9f556106b1b26b93c419b13eb3
ivanbumbuc <bumbukk.bumbuk@gmail.com>
2022-11-23 11:52:25 +0200

    Lab 4

--- MyBook/src/book/Image.java
+++ MyBook/src/book/Image.java
@@ -1,12 +1,18 @@
 package book;
 
+import java.util.concurrent.TimeUnit;
+
 public class Image implements Element{
     private String imageName;
 
-    public Image(String imageName) {
-        this.imageName = imageName;
+    public Image(String name) {
+        imageName = name;
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-
     @Override
     public void print()
     {
